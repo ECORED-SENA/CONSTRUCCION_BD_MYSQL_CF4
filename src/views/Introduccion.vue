8379ef02d7bf720fbed7017fba0ae8f9beeaa869
Leoska89 <leoska890@gmail.com>
2021-05-27 10:10:54 -0500

    cambio link video introducción

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,10 +11,7 @@
     
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/vPmz2UCHYsg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      
-      
-
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/Jzoj-o-R3x4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       
 
 </template>
